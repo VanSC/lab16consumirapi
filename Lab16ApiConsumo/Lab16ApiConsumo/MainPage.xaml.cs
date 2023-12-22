4fc2d6a8fd4ff235f6bc5e2726531289c474918a
VanSC <vanessa.choque@tecsup.edu.pe>
2023-12-22 01:36:08 -0500

    first commit

---
+++ Lab16ApiConsumo/Lab16ApiConsumo/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Lab16ApiConsumo.models;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Lab16ApiConsumo
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private const string BaseUrl = "https://peliculas-api-gamma.vercel.app";
+
+        public async Task<ApiResponse> GetAsync(string endpoint)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+
+                HttpResponseMessage response = await client.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    ApiResponse result = JsonConvert.DeserializeObject<ApiResponse>(json);
+                    return result;
+                }
+                else
+                {
+                    throw new Exception($"Error en la solicitud: {response.StatusCode}");
+                }
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                ApiResponse response = await GetAsync("/peliculas");
+
+                var movieList = response.results.Select(movie => new
+                {
+                    Title = movie.title,
+                    Imagen = movie.image,
+                    Author = movie.author,
+                    Genre = movie.genre,
+                }).ToList();
+
+                CollectionViewDemo.ItemsSource = movieList;
+            }
+            catch (Exception ex)
+            {
+                // Manejar errores
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Lab16ApiConsumo/Lab16ApiConsumo/models/ApiResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab16ApiConsumo.models
+{
+    public class Pelicula
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string genre { get; set; }
+        public string image { get; set; }
+    }
+    public class ApiResponse
+    {
+        public List<Pelicula> results { get; set; }
+    }
+}
